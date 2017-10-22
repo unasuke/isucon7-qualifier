f89d1836fc37993a1361482ce741bb2b48ccd891
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-10-22 14:35:09 +0900

    set -e

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # $1 -> deployment branch
 branch=$1
 app_server_1='27.133.154.88'
--- deploy/application.sh
+++ deploy/application.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 git fetch origin $1
 git checkout origin/$1
 sudo systemctl restart isubata.ruby.service
