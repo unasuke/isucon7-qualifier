a1d658c6affdc7b41927eaae32830eb3187e729b
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-10-22 17:17:45 +0900

    Fix query

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -119,21 +119,21 @@ class App < Sinatra::Base
     channel_id = params[:channel_id].to_i
     last_message_id = params[:last_message_id].to_i
     statement = db.prepare(
-      'SELECT message.id, message.created_at, message.content, user.id, user.display_name, user.avatar_icon FROM message INNER JOIN user ON user.id = message.user_id WHERE message.id > ? AND message.channel_id = ? ORDER BY message.id DESC LIMIT 100'
+      'SELECT message.id, message.created_at, message.content, message.user_id, user.name as user_name, user.display_name as display_name, user.avatar_icon as avatar_icon FROM message INNER JOIN user ON user.id = message.user_id WHERE message.id > ? AND message.channel_id = ? ORDER BY message.id DESC LIMIT 100'
     )
     rows = statement.execute(last_message_id, channel_id).to_a
     response = []
     rows.each do |row|
       r = {}
-      r['id'] = row['message.id']
-      r['user'] = row['user.id']
-      r['date'] = row['message.created_at'].strftime("%Y/%m/%d %H:%M:%S")
-      r['content'] = row['message.content']
+      r['id'] = row['id']
+      r['user'] = { display_name: row['display_name'], avatar_icon: row['avatar_icon'], name: row['user_name'] }
+      r['date'] = row['created_at'].strftime("%Y/%m/%d %H:%M:%S")
+      r['content'] = row['content']
       response << r
     end
     response.reverse!
 
-    max_message_id = rows.empty? ? 0 : rows.map { |row| row['message.id'] }.max
+    max_message_id = rows.empty? ? 0 : rows.map { |row| row['id'] }.max
     statement = db.prepare([
       'INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at) ',
       'VALUES (?, ?, ?, NOW(), NOW()) ',
@@ -196,17 +196,17 @@ class App < Sinatra::Base
 
     n = 20
     statement = db.prepare(
-      'SELECT message.id, message.created_at, message.content, user.id, user.display_name, user.avatar_icon FROM message INNER JOIN user ON user.id = message.user_id WHERE message.channel_id = ? ORDER BY message.id DESC LIMIT 100 OFFSET ?'
+      'SELECT message.id, message.created_at, message.content, message.user_id, user.name as user_name, user.display_name as display_name, user.avatar_icon as avatar_icon FROM message INNER JOIN user ON user.id = message.user_id WHERE message.channel_id = ? ORDER BY message.id DESC LIMIT ? OFFSET ?'
     )
     rows = statement.execute(@channel_id, n, (@page - 1) * n).to_a
     statement.close
     @messages = []
     rows.each do |row|
       r = {}
-      r['id'] = row['message.id']
-      r['user'] = row['message.user_id']
-      r['date'] = row['message.created_at'].strftime("%Y/%m/%d %H:%M:%S")
-      r['content'] = row['message.content']
+      r['id'] = row['id']
+      r['user'] = { display_name: row['display_name'], avatar_icon: row['avatar_icon'], name: row['user_name'] }
+      r['date'] = row['created_at'].strftime("%Y/%m/%d %H:%M:%S")
+      r['content'] = row['content']
       @messages << r
     end
     @messages.reverse!
@@ -241,7 +241,7 @@ class App < Sinatra::Base
     @self_profile = user['id'] == @user['id']
     erb :profile
   end
-  
+
   get '/add_channel' do
     if user.nil?
       return redirect '/login', 303
